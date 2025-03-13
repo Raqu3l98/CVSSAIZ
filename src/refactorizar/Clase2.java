88a6d3fa6618a1439f4106bc2471d8972f0770db
Raqu3l98 <raquelsaizlozano@gmail.com>
2025-03-13 14:06:57 +0100

    Segundo commit

---
+++ src/refactorizar/Clase2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package refactorizar;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase2 {
+    public void visualizar(){
+        System.out.println("Estoy en la Clase 2");
+        System.out.println("Estoy en la rama de PRODUCCIÓN");
+    }
+}
