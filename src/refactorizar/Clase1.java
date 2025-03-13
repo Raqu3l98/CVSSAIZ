b16227b3645ece3264ba96c98699493f292a9f68
Raqu3l98 <raquelsaizlozano@gmail.com>
2025-03-13 13:54:50 +0100

    Pirmer commit

---
+++ src/refactorizar/Clase1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package refactorizar;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase1 {
+     private String nombre="Raqule Saiz Lozano";
+     public void visualizar(){
+            System.out.println("Estoy en la clase 1");
+}
+     
+     
+}
